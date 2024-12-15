3b56f3d6786e4b079446bbe4bb19ec338f4baae3
AJxles <jules.aguesse@hotmail.com>
2024-12-15 02:59:48 +0100

    HeaderMobilié

--- vite-project/client/src/pages/Mobilier.jsx
+++ vite-project/client/src/pages/Mobilier.jsx
@@ -1,11 +1,12 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import HeaderMobilié from "../components/HeaderMobilié"
 
 function Mobilier() {
   return (
     <>
       <Header />
-
+      <HeaderMobilié/>
       <Footer />
     </>
   );
